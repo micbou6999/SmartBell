4ce07cafd51b8305519bb7782768dea2b5fb893a
Michael Boutros <michaelboutros@Michaels-MacBook-Pro-2.local>
2023-07-25 02:10:37 -0700

    updated

--- App Software/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package Robin.Do
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- App Software/lib/index.dart
+++
@@ -1,7 +0,0 @@
-// Export pages
-export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/pages/profile_page/profile_page_widget.dart' show ProfilePageWidget;
-export '/pages/trackers/video_footage/video_footage_widget.dart'
-    show VideoFootageWidget;
-export '/pages/onboarding/login/login_widget.dart' show LoginWidget;
-export '/edit_profile/edit_profile_widget.dart' show EditProfileWidget;
